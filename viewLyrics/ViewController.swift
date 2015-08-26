f2ae7f05da2709afc4e7d66a1550a0b2fa08c517
Ryan Rudzitis <ryanrudzitis@icloud.com>
2015-08-26 13:22:25 -0400

    Initial Commit

---
+++ viewLyrics/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  viewLyrics
+//
+//  Created by Ryan Rudzitis on 2015-08-26.
+//  Copyright © 2015 Ryan Rudzitis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
